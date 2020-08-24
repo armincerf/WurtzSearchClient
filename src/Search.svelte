2f7e1fc2987ff2496b81a0823032c825b732a62a
Alex Davis <alx@juxt.pro>
2020-08-24 16:21:03 +0100

    Add load more button and fix mobile styles
    Tried to do an infinite scroll thing but couldn't get it to work.. need to read
    up on how svelte works I guess..
    
    Also formatted the file, still learning vs code sorry

--- src/Search.svelte
+++ src/Search.svelte
@@ -1,68 +1,106 @@
 <script>
-import { onMount } from 'svelte';
-import algoliasearch from 'algoliasearch/lite';
+  import { onMount } from "svelte";
+  import algoliasearch from "algoliasearch/lite";
 
-let searchClient;
-let index;
+  let searchClient;
+  let index;
 
-let query = '';
-let hits = [];
-let appID = 'XMYY7X6YSY';
-let apiKey = 'e78bc0e48fb18f5972e9e8710d269911';
-let indexName = 'bill questions' 
-let dateTimeFormat = new Intl.DateTimeFormat('en-us', {
-  year: '2-digit',
-  month: '2-digit',
-  day: '2-digit',
-  hour: 'numeric',
-  minute: 'numeric',
-  hour12: true,
-  timeZone: 'EST'
-});
+  let query = "";
+  let hits = [];
+  let extraHits = [];
+  let appID = "XMYY7X6YSY";
+  let apiKey = "e78bc0e48fb18f5972e9e8710d269911";
+  let indexName = "test_questions";
+  let dateTimeFormat = new Intl.DateTimeFormat("en-us", {
+    year: "2-digit",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+    timeZone: "EST",
+  });
 
-function formatted_date(timestamp) {
+  function formatted_date(timestamp) {
     let date = Date.parse(timestamp) + 360 * 60 * 1000;
     //jesus i hate javascript
-    let [{ value: month },,{ value: day },,{ value: year },,{ value: hour},,{ value: minute},,{ value: hour12}]
-     = dateTimeFormat.formatToParts(date);
-    return `${month}.${day}.${year} ${hour}:${minute} ${hour12}`
-} 
+    let [
+      { value: month },
+      ,
+      { value: day },
+      ,
+      { value: year },
+      ,
+      { value: hour },
+      ,
+      { value: minute },
+      ,
+      { value: hour12 },
+    ] = dateTimeFormat.formatToParts(date);
+    return `${month}.${day}.${year} ${hour}:${minute} ${hour12}`;
+  }
 
-onMount(() => {
+  onMount(() => {
+    searchClient = algoliasearch(appID, apiKey);
 
-	searchClient = algoliasearch(
-        appID,
-        apiKey
-	);
+    index = searchClient.initIndex(indexName);
+  });
 
-	index = searchClient.initIndex(indexName);
-
-	// Warm up search
-	index.search({ query }.query).then(console.log)
-
-});
-
-// Fires on each keyup in form
-async function search() {
-	const result = await index.search({ query }.query);
-	hits = result.hits;
-	console.log(hits)
-}
+  // Fires on each keyup in form
+  async function search() {
+    const result = await index.search({ query }.query, {
+      hitsPerPage: 100,
+    });
+    hits = result.hits;
+  }
+  async function loadMore() {
+    const result = await index.search(query, {
+      hitsPerPage: 1000,
+    });
+    extraHits = result.hits;
+  }
 </script>
 
-<h1>ea<span class="blue">s</span>rch</h1>
+<h1>
+  ea
+  <span class="blue">s</span>
+  rch
+</h1>
 search for things
 <div>
-	<input type="text" bind:value={query} on:keyup={search}>
+  <input type="text" bind:value={query} on:keyup={search} />
 </div>
 
-
 {#each hits as hit}
-	<section> 
-        <h3>
-            <dco>{formatted_date(hit.timestamp)}</dco>
-            <qco>{@html hit._highlightResult.question.value}</qco>
-        </h3>
-        <p class="answer">{@html hit._highlightResult.answer.value}</p>
-	</section>
+  {#if hit.question}
+    <section>
+      <h3>
+        <dco>{formatted_date(hit.timestamp)}</dco>
+        <qco>
+          {@html hit._highlightResult.question.value}
+        </qco>
+      </h3>
+      <p class="answer">
+        {@html hit._highlightResult.answer.value}
+      </p>
+    </section>
+  {/if}
+{/each}
+{#if hits.length && !extraHits.length}
+  <button on:click={loadMore}>load more</button>
+{/if}
+{#each extraHits as hit}
+  {#if hit.question}
+    <section>
+      <h3>
+        <dco>{formatted_date(hit.timestamp)}</dco>
+        <qco>
+          {@html hit._highlightResult.question.value}
+        </qco>
+      </h3>
+      <p class="answer">
+        {@html hit._highlightResult.answer.value}
+      </p>
+    </section>
+  {/if}
 {/each}
